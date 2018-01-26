c2049dc0b74a4b705fb6f008679e060f2cc43617
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-01-26 08:22:51 -0800

    config link fixed

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ module.exports = {
     PORT: process.env.PORT || 8080, 
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000', 
     DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/treemaker-database',
-    DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/treemaker-test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/treemaker-test'
 }
