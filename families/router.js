c9ba1189f0bdfaf7c8ca9b797802a8cdabce1728
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-06 14:29:53 -0800

    totalMember validation complete

--- families/router.js
+++ families/router.js
@@ -126,10 +126,6 @@ router.post('/', jwtAuth, jsonParser, (req, res) => {
             return Family.hashPassword(password); 
         })
         .then(hash => {
-            console.log("CHECKOUTPOINT AAA")
-            console.log(`Username: ${username}`)
-            console.log(`FamilyName: ${family_name}`)
-            console.log(`Password: ${hash}`)
             return Family.create({
                 family_name, 
                 password: hash, 
@@ -164,79 +160,94 @@ router.get('/:id', jsonParser, (req, res) => {
 
 router.post('/:id/members', jsonParser, (req, res) => {
 
-    // validates presence of relative data
-
-    let parents, children, siblings, pictures; 
-
-    if (!(req.body.parents || 
-        req.body.children || 
-        req.body.siblings || 
-        req.body.pictures)) {
-        res.status(404).json({
-            code: 422, 
-            reason: "Validation Error", 
-            message: "Must supply parent, child or sibling",   
-        })
-    }
-
-    // creates variables for optional data
-
-    if(req.body.parents) {
-        parents = [
-            { parent_id: req.body.parents }
-        ]
-    }
-
-    if(req.body.siblings) {
-        siblings = [
-            { sibling_id: req.body.siblings }
-        ]
-    }
-
-    if(req.body.children) {
-        children = [
-            { children_id: req.body.children }
-        ]
-    }
-
-    if(req.body.url) {
-        url = [
-            { url: req.body.url }
-        ]
-    }
-
-    const newMember = {
-        fname: req.body.fname, 
-        lname: req.body.lname, 
-        mname: req.body.mname, 
-        birth_date: req.body.birth_date,
-        birth_town: req.body.birth_town, 
-        birth_state: req.body.birth_state,  
-        death_date: req.body.death_date, 
-        death_town: req.body.death_town, 
-        death_state: req.body.death_state, 
-        short_bio: req.body.short_bio, 
-        long_bio: req.body.long_bio, 
-        parents, 
-        siblings, 
-        children,
-        pictures
-    }
-
-    console.log("NEW MEMBER", newMember, req.params.id)
+    // checks current number of members in family 
 
     Family
-        .update(
-            { _id: req.params.id }, 
-            { $push: { members: newMember }}
-        )
+        .findById(req.params.id)
         .then(family => {
-            res.sendStatus(204)
+            let totalMembers = family.members.length
+            return totalMembers 
         })
-        .catch(err => {
-            console.error(err)
-            res.status(500).json({err}); 
-        });
+        .then(totalMembers => {
+
+            // validates presence of relative data
+
+            let parents, children, siblings, pictures; 
+            
+            if (!(req.body.parents || req.body.children || 
+                req.body.siblings || req.body.pictures) &&
+                totalMembers >= 1
+            ) {
+                return res.status(404).json({
+                    code: 422, 
+                    reason: "Validation Error", 
+                    message: "Must supply parent, child or sibling",   
+                })
+            }
+
+            // creates variables for optional data
+
+            if(req.body.parents) {
+                parents = [
+                    { parent_id: req.body.parents }
+                ]
+            }
+
+            if(req.body.siblings) {
+                siblings = [
+                    { sibling_id: req.body.siblings }
+                ]
+            }
+
+            if(req.body.children) {
+                children = [
+                    { children_id: req.body.children }
+                ]
+            }
+
+            if(req.body.url) {
+                url = [
+                    { url: req.body.url }
+                ]
+            }
+
+            const newMember = {
+                fname: req.body.fname, 
+                lname: req.body.lname, 
+                mname: req.body.mname, 
+                birth_date: req.body.birth_date,
+                birth_town: req.body.birth_town, 
+                birth_state: req.body.birth_state,  
+                death_date: req.body.death_date, 
+                death_town: req.body.death_town, 
+                death_state: req.body.death_state, 
+                short_bio: req.body.short_bio, 
+                long_bio: req.body.long_bio, 
+                parents, 
+                siblings, 
+                children,
+                pictures
+            }
+
+            console.log("NEW MEMBER", newMember, req.params.id)
+
+            Family
+                .update(
+                    { _id: req.params.id }, 
+                    { $push: { members: newMember }}
+                )
+                .then(family => {
+                    res.sendStatus(204)
+                })
+                .catch(err => {
+                    console.error(err)
+                    res.sendStatus(500);  
+                });
+            })
+            .catch(err => {
+                console.error(err); 
+                res.sendStatus(500); 
+            }); 
 })
 
 
